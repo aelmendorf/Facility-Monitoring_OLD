9f547bbfbeae6300ec3606d0415f270222673d63
Andrew Elmendorf <aelmendorf234@gmail.com>
2019-09-11 16:53:21 -0400

    testing

--- FacilityMonitoring.ConsoleTesting/Program.cs
+++ FacilityMonitoring.ConsoleTesting/Program.cs
@@ -45,5 +45,105 @@ namespace FacilityMonitoring.ConsoleTesting
             }
             Console.ReadKey();
         }
+
+        public static void TestWarning() {
+            using (var context = new FacilityContext()) {
+                var device = context.ModbusDevices
+                    .OfType<GenericMonitorBox>()
+                    .AsNoTracking()
+                    .Include(e => e.Registers)
+                        .ThenInclude(e => e.SensorType)
+                    .Include(e => e.Readings)
+                    .FirstOrDefault(e => e.Identifier == "GasBay");
+                if (device != null) {
+                    MonitorBoxOperations operations = new MonitorBoxOperations(device);
+                    if (!operations.SetWarning(true)) {
+                        Console.WriteLine("Done! Press any key to exit");
+                    } else {
+                        Console.WriteLine("Error Send Failed");
+                    }
+                } else {
+                    Console.WriteLine("Error: Device Not Found");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void TestMaintenance() {
+            using (var context = new FacilityContext()) {
+                var device = context.ModbusDevices
+                    .OfType<GenericMonitorBox>()
+                    .AsNoTracking()
+                    .Include(e => e.Registers)
+                        .ThenInclude(e => e.SensorType)
+                    .Include(e => e.Readings)
+                    .FirstOrDefault(e => e.Identifier == "GasBay");
+                if (device != null) {
+                    MonitorBoxOperations operations = new MonitorBoxOperations(device);
+                    if (!operations.SetMaintenance(true)) {
+                        Console.WriteLine("Done! Press any key to exit");
+                    } else {
+                        Console.WriteLine("Error Send Failed");
+                    }
+                } else {
+                    Console.WriteLine("Error: Device Not Found");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void TestAlarm() {
+            using (var context = new FacilityContext()) {
+                var device = context.ModbusDevices
+                    .OfType<GenericMonitorBox>()
+                    .AsNoTracking()
+                    .Include(e => e.Registers)
+                        .ThenInclude(e => e.SensorType)
+                    .Include(e => e.Readings)
+                    .FirstOrDefault(e => e.Identifier == "GasBay");
+                if (device != null) {
+                    MonitorBoxOperations operations = new MonitorBoxOperations(device);
+                    if (!operations.SetAlarm(true)) {
+                        Console.WriteLine("Done! Press any key to exit");
+                    } else {
+                        Console.WriteLine("Error Send Failed");
+                    }
+                } else {
+                    Console.WriteLine("Error: Device Not Found");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void TestRead() {
+            using (var context = new FacilityContext()) {
+                var device = context.ModbusDevices
+                    .OfType<GenericMonitorBox>()
+                    .AsNoTracking()
+                    .Include(e => e.Registers)
+                        .ThenInclude(e => e.SensorType)
+                    .Include(e => e.Readings)
+                    .FirstOrDefault(e => e.Identifier == "GasBay");
+                if (device != null) {
+                    MonitorBoxOperations operations = new MonitorBoxOperations(device);
+                    var reading = (GenericBoxReading)operations.ReadAll();
+                    if (reading != null) {
+                        foreach (var register in device.Registers.OfType<AnalogChannel>().OrderBy(e => e.RegisterIndex)) {
+                            Console.WriteLine("{0}: {1}", register.Name, Convert.ToDouble(reading[register.PropertyMap]));
+                        }
+                        device.Readings.Add(reading);
+                        context.Entry<ModbusDevice>(device).State = EntityState.Modified;
+                        context.Readings.Add(reading);
+                        context.SaveChanges();
+                        Console.WriteLine("Done! Press any key to exit");
+                    } else {
+                        Console.WriteLine("Error Reading Failed");
+                    }
+                } else {
+                    Console.WriteLine("Error: Device Not Found");
+                }
+            }
+            Console.ReadKey();
+        }
     }
 }
