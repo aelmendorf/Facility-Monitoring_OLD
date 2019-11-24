f029f2049f8e7c7174524799819d00ad4385585d
Andrew Elmendorf <aelmendorf234@gmail.com>
2019-11-24 14:22:50 -0500

    arduinoDebugger

---
+++ FacilityMonitoring.ArduinoDebugger/Common/AnalogModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacilityMonitoring.ArduinoDebugger.Common {
+    public class AnalogModel {
+        public string Channel { get; set; }
+        public double Value { get; set; }
+
+    }
+}
---
+++ FacilityMonitoring.ArduinoDebugger/Common/Regions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacilityMonitoring.ArduinoDebugger.Common {
+    public static class Regions {
+        public static string MainRegion { get => "MainRegion";}
+        public static string AnalogRegion { get => "AnalogRegion"; }
+        public static string Digital24Region { get => "Digital24Region"; }
+        public static string DigitalPullupRegion { get => "DigitalPullupRegion"; }
+    }
+}
